88ab800af698597f40c7f61e0e843dabcb5cb208
nav-commits <nav_d17@hotmail.com>
2022-10-12 14:57:40 -0400

    added form validation for profile page

--- src/Components/Atoms/SelectField/SelectField.jsx
+++ src/Components/Atoms/SelectField/SelectField.jsx
@@ -4,12 +4,14 @@ import "./SelectField.css";
 function SelectField(props) {
 const width = props.width
 const provincesData = props.provincesData
+const value = props.value
+const handleInputChange = props.handleInputChange
   return (
     <>
-      <select style={{ width: width }} className="select-field">
+      <select value={value} style={{ width: width }} onChange={handleInputChange} className="select-field">
       <option value="" disabled selected>Select one</option>
-       {provincesData.map((data)=>{
-        return <option value={data}>{data}</option>
+       {provincesData.map((data,index)=>{
+        return <option key={index} value={data}>{data}</option>
        })}
       </select>
     </>
--- src/Components/Views/PersonalInformation.jsx
+++ src/Components/Views/PersonalInformation.jsx
@@ -6,15 +6,65 @@ import { StepperContext } from "../../Context/StepperContext";
 import TextField from "../Atoms/TextField/TextField";
 import FormSubmissionButton from "../Atoms/FromSubmissionButton/FormSubmissionButton";
 import SelectField from "../Atoms/SelectField/SelectField";
-import {provincesData} from "../../Data/Provinces"
+import { provincesData } from "../../Data/Provinces";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
 function PersonalInformation() {
   const { activeStep, nextStep } = useContext(StepperContext);
+  // const [select, setSelect] = useState();
+  const [submitted, setSubmitted] = useState(false);
+  const [postalCodeError, setPostalCodeError] = useState("");
+  const [personalValues, setPersonalValues] = useState({
+    streetAddress: "",
+    streetAddressLineTwo: "",
+    city: "",
+    province: "",
+    postalCode: "",
+  });
+
+  const streetAddressInputChange = (event) => {
+    console.log(personalValues.streetAddress);
+    setPersonalValues({ ...personalValues, streetAddress: event.target.value });
+  };
+
+  const streetAddressLineTwoInputChange = (event) => {
+    setPersonalValues({
+      ...personalValues,
+      streetAddressLineTwo: event.target.value,
+    });
+  };
+  const cityInputChange = (event) => {
+    setPersonalValues({ ...personalValues, city: event.target.value });
+  };
+
+  const provinceInputChange = (event) => {
+    setPersonalValues({ ...personalValues, province: event.target.value });
+  };
+
+  const postalCodeInputChange = (event) => {
+    setPersonalValues({ ...personalValues, postalCode: event.target.value });
+  };
+
+  const regexPostalCode= /[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]/
 
   const handleSubmit = (event) => {
-    alert(`it worked`);
+    if (
+      (personalValues.streetAddress &&
+        personalValues.city &&
+        personalValues.province &&
+        personalValues.postalCode  &&
+        regexPostalCode.test(personalValues.postalCode)) ||
+       personalValues.streetAddressLineTwo
+    ) {
+      setPostalCodeError("")
+      nextStep();
+    }
+    else if (!regexPostalCode.test(personalValues.postalCode)){
+      setPostalCodeError("The postal code format is incorrect")
+    }
+   
+    setSubmitted(true);
     event.preventDefault();
-    nextStep();
   };
 
   const [matches, setMatches] = useState(
@@ -35,28 +85,206 @@ function PersonalInformation() {
         form={
           <form action="" onSubmit={handleSubmit}>
             <div className="text-field-container">
-              <label style={{fontSize:'0.8rem', fontWeight:'600'}}  htmlFor="">Street Address</label>
-              <TextField width={matches ? "27rem" : null} />
+              <label
+                style={{ fontSize: "0.8rem", fontWeight: "600" }}
+                htmlFor=""
+              >
+                Street Address
+              </label>
+              <TextField
+                width={matches ? "27rem" : null}
+                value={personalValues.streetAddress}
+                handleInputChange={streetAddressInputChange}
+              />
+              {submitted && !personalValues.streetAddress ? (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "2px",
+                    color: "#cb061d",
+                  }}
+                >
+                  {" "}
+                  <ErrorOutlineIcon />{" "}
+                  <p
+                    style={{
+                      color: "#cb061d",
+                      fontSize: "0.8rem",
+                    }}
+                  >
+                    This field is required
+                  </p>
+                </div>
+              ) : null}
             </div>
+
             <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-              <div style={{ display: "flex", flexDirection: "column",gap: "10px", marginTop:'10px'   }}>
-                <label style={{fontSize:'0.8rem',  fontWeight:'600'}} htmlFor="">Address Line 2(optional)</label>
-                <TextField width={matches ? "12.78rem" : "20.78rem"} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                  marginTop: "10px",
+                }}
+              >
+                <label
+                  style={{ fontSize: "0.8rem", fontWeight: "600" }}
+                  htmlFor=""
+                >
+                  Address Line 2(optional)
+                </label>
+                <TextField
+                  width={matches ? "12.78rem" : "20.78rem"}
+                  value={personalValues.streetAddressLineTwo}
+                  handleInputChange={streetAddressLineTwoInputChange}
+                />
               </div>
-              <div style={{ display: "flex", flexDirection: "column",gap: "10px", marginTop:'10px'  }}>
-                <label style={{fontSize:'0.8rem',  fontWeight:'600'}}  htmlFor="">City</label>
-                <TextField width={matches ? "12.78rem" : "20.78rem"} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                  marginTop: "10px",
+                }}
+              >
+                <label
+                  style={{ fontSize: "0.8rem", fontWeight: "600" }}
+                  htmlFor=""
+                >
+                  City
+                </label>
+                <TextField
+                  width={matches ? "12.78rem" : "20.78rem"}
+                  value={personalValues.city}
+                  handleInputChange={cityInputChange}
+                />
+                {submitted && !personalValues.city ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "2px",
+                      color: "#cb061d",
+                    }}
+                  >
+                    {" "}
+                    <ErrorOutlineIcon />{" "}
+                    <p
+                      style={{
+                        color: "#cb061d",
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      This field is required
+                    </p>
+                  </div>
+                ) : null}
               </div>
             </div>
 
             <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-              <div style={{ display: "flex", flexDirection: "column",gap: "10px", marginTop:'10px'   }}>
-                <label style={{fontSize:'0.8rem',  fontWeight:'600'}}  htmlFor="">Province</label>
-                <SelectField width={matches ? "13.78rem" : "20.78rem"}  provincesData = {provincesData}/>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                  marginTop: "10px",
+                }}
+              >
+                <label
+                  style={{ fontSize: "0.8rem", fontWeight: "600" }}
+                  htmlFor=""
+                >
+                  Province
+                </label>
+                <SelectField
+                  width={matches ? "13.78rem" : "20.78rem"}
+                  provincesData={provincesData}
+                  value={personalValues.province}
+                  handleInputChange={provinceInputChange}
+                />
+                {submitted && !personalValues.province ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "2px",
+                      color: "#cb061d",
+                    }}
+                  >
+                    {" "}
+                    <ErrorOutlineIcon />{" "}
+                    <p
+                      style={{
+                        color: "#cb061d",
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      This field is required
+                    </p>
+                  </div>
+                ) : null}
               </div>
-              <div style={{ display: "flex", flexDirection: "column",gap: "10px", marginTop:'10px'   }}>
-                <label style={{fontSize:'0.8rem',  fontWeight:'600'}}  htmlFor="">Postal Code</label>
-                <TextField width={matches ? "12.78rem" : "20.78rem"} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                  marginTop: "10px",
+                }}
+              >
+                <label
+                  style={{ fontSize: "0.8rem", fontWeight: "600" }}
+                  htmlFor=""
+                >
+                  Postal Code
+                </label>
+                <TextField
+                  width={matches ? "12.78rem" : "20.78rem"}
+                  value={personalValues.postalCode}
+                  handleInputChange={postalCodeInputChange}
+                />
+                {submitted && !personalValues.postalCode ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "2px",
+                      color: "#cb061d",
+                    }}
+                  >
+                    {" "}
+                    <ErrorOutlineIcon />{" "}
+                    <p
+                      style={{
+                        color: "#cb061d",
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      This field is required
+                    </p>
+                  </div>
+                ) : null}
+                {postalCodeError? <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "2px",
+                      color: "#cb061d",
+                    }}
+                  >
+                    {" "}
+                    <ErrorOutlineIcon />{" "}
+                    <p
+                      style={{
+                        color: "#cb061d",
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      {postalCodeError}
+                    </p>
+                  </div>: null}
               </div>
             </div>
 
