5294a35c49506b5ac816304cf26dc32402a5a3dc
d4cho <danielcho91@gmail.com>
2023-11-24 11:15:55 -0500

    add bio widget

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import BioStorybookWidget from "bio-storybook-widget";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BioStorybookWidget projectSource={"eqbank"} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
